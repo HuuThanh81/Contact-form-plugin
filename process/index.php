b7e24921279e56376ac92fa81253c7f779d9e80b
HuuThanh <huynhlehuuthanh@gmail.com>
2021-09-27 13:27:01 +0700

    :left_speech_bubble: Show-Comments-Form-Submit

--- process/index.php
+++ process/index.php
@@ -3,6 +3,12 @@ $path = preg_replace('/wp-content.*$/','', __DIR__);
 require_once($path."wp-load.php");
 
 global $wpdb, $post;
+
+if(array_key_exists('submit_scripts_update',$_POST)){
+    $x = sanitize_email( $_POST['sc_email']);
+    $y = sanitize_textarea_field( $_POST['sc_subject']);
+}
+
 if(isset($_POST['ContactSubmit']) && $_POST['ContactSubmit']=="1")
 {   
     $name = sanitize_text_field( $_POST['name']);
@@ -13,10 +19,9 @@ if(isset($_POST['ContactSubmit']) && $_POST['ContactSubmit']=="1")
     $to = 'huynhlehuuthanh@gmail.com';
     $subject ='hello !';
     $message = '';
-    $message .= 'Name:'.$name.'<br/>';
-    $message .= 'email:'.$email.'<br/>';
-    $message .= 'phone:'.$phone.'<br/>';
-    $message .= 'Name:'.$name.'<br/>';
+    $message .= 'Name: '.$name.'<br/>';
+    $message .= 'Email: '.$email.'<br/>';
+    $message .= 'Phone: '.$phone.'<br/>';
 
     $comments = wpautop( $comments);
     $comments = str_replace("<p>","",$comments);
@@ -25,16 +30,6 @@ if(isset($_POST['ContactSubmit']) && $_POST['ContactSubmit']=="1")
     $message .= 'Comments:<br/>'. $comments . '<br/><br/>';
     $message .= 'Thank you.';
     wp_mail($to, $subject, $message);
-
-    $time = current_time('mysql');
-    $data = array(
-        'comment_post_ID' => $post->ID,
-        'comment_content' => $message,
-        'comment_author_IP' => $_SERVER['REMOTE_ADDR'],
-        'comment_date' => $time,
-        'comment_approved' => 1
-    );
-    wp_insert_comment($data);
     
     $insertData = $wpdb -> get_results("INSERT INTO ".$wpdb->prefix."form_submissions (name, email, phone, message) VALUES ('".$name."','".$email."','".$phone."','".$comments."') ");
 
@@ -43,6 +38,16 @@ if(isset($_POST['ContactSubmit']) && $_POST['ContactSubmit']=="1")
     $return ['message'] = 'Your information has been received.';
 
     echo json_encode($return);
+
+    $time = current_time('mysql');
+    $data = array(
+        // 'comment_post_ID' => $post->ID,
+        'comment_content' => $message,
+        'comment_author_IP' => $_SERVER['REMOTE_ADDR'],
+        'comment_date' => $time,
+        'comment_approved' => 1
+    );
+    wp_insert_comment($data);
 }
 
 
